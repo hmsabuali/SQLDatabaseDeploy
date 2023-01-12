4d09b166770fe17cbeb76192c3a219e3243c7f1d
HASEN SADA <hmsada@HasenSada.onmicrosoft.com>
2023-01-11 22:40:44 -0500

    Add project files.

---
+++ SQLDatabaseDeploy/dbo/Tables/Episode.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Episode] (
+    [EpisodeId]   INT           NOT NULL,
+    [EpisodeName] VARCHAR (100) NULL,
+    [Seriesid]    INT           NULL,
+    CONSTRAINT [PK_Episode] PRIMARY KEY CLUSTERED ([EpisodeId] ASC)
+);
+
---
+++ SQLDatabaseDeploy/dbo/Tables/Series.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Series] (
+    [SeriesId]   INT          NOT NULL,
+    [SeriesName] VARCHAR (50) NULL,
+    [StartDate]  DATE         NULL
+);
+
